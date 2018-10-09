c69ef35f3bd847a2df9995ef69105f07ce34ceed
ChanduValiveti <cvalivet@ford.com>
2018-10-09 11:15:24 -0400

    Added the environment variables and modified test cases

--- src/main/java/io.pivotal.pal.tracker/WelcomeController.java
+++ src/main/java/io.pivotal.pal.tracker/WelcomeController.java
@@ -4,17 +4,19 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class WelcomeController {
 
     private String greeting;
 
-    public WelcomeController(@Value("${WELCOME_MESSAGE:hello}") String greeting) {
+    public WelcomeController(@Value("${WELCOME_MESSAGE}") String greeting) {
+
         this.greeting = greeting;
     }
 
     @GetMapping("/")
     public String sayHello() {
-        return greeting;
+        return this.greeting;
     }
 }
